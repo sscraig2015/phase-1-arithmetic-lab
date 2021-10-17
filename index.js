21dccf4c5f6d1fd950f9f3b2e6fecf091d90ff0e
Sean <craig.sean92@gmail.com>
2021-10-17 14:36:00 -0600

    commit tree 06617ea9d7f72117867d0601c41bfdbeb5f3091a

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var num1 = 31;
+var num2 = 2;
+var multiply = num1 * num2;
 
-// Write your code here
+var random = Math.floor(Math.random() * 10) + 1
+
+var num3 = 16  
+var num4 = 4
+var mod = num3/num4
+
+var max = Math.max(1,2,20);
